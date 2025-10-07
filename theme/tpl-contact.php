7f26585bbe54fc42bb18d3708d5b10ccfad16c32
Farashev Dzhulian <dzhulianfarashev@gmail.com>
2025-10-07 11:31:46 -0400

    fin formatif

---
+++ theme/tpl-contact.php
@@ -0,0 +1,39 @@
+<?php /* Template Name: Liste des recettes */ ?>
+<?php get_header(); ?>
+
+<div class="wrapper">
+
+<div class="hero">
+<div class="hero-media">
+</div>
+<div class="hero-content">
+<h1>
+Contactez-nous
+</h1>
+</div>
+</div>
+
+<section class="coordonees">
+<div class="colonnes-coordonees">
+
+<div class="premiere-colonne">
+<div class="titre-colonne">
+    <svg class="icon">
+        <use href="#icon-iconName"></use>
+    </svg> <h2>Horaire d’ouverture</h2>
+</div>
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+</section>
+
+
+</div>
